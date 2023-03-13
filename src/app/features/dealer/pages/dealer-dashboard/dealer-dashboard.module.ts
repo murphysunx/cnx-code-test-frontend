0ba7115edf5a482b1845ef4dbbf7608dfec91cde
Murphy Sun <murphysunx@gmail.com>
2023-03-13 14:53:50 +1100

    #style: improve loading UI

--- src/app/features/dealer/pages/dealer-dashboard/dealer-dashboard.module.ts
+++ src/app/features/dealer/pages/dealer-dashboard/dealer-dashboard.module.ts
@@ -4,9 +4,10 @@ import { DealerListModule } from '../../components/dealer-list/dealer-list.modul
 import { DealerDashboardComponent } from './dealer-dashboard.component';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { LoadingPageModule } from 'src/app/shared/components/loading-page/loading-page.module';
 
 @NgModule({
-  imports: [CommonModule, DealerListModule, MatButtonModule],
+  imports: [CommonModule, DealerListModule, MatButtonModule, LoadingPageModule],
   exports: [],
   declarations: [DealerDashboardComponent],
 })
--- src/app/features/vehicle/pages/dealer-vehicle/dealer-vehicle.module.ts
+++ src/app/features/vehicle/pages/dealer-vehicle/dealer-vehicle.module.ts
@@ -5,9 +5,16 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { DealerVehicleComponent } from './dealer-vehicle.component';
+import { LoadingPageModule } from 'src/app/shared/components/loading-page/loading-page.module';
 
 @NgModule({
-  imports: [CommonModule, VehicleDetailModule, MatListModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    VehicleDetailModule,
+    MatListModule,
+    MatButtonModule,
+    LoadingPageModule,
+  ],
   exports: [DealerVehicleComponent],
   declarations: [DealerVehicleComponent],
 })
